1b0a2956d9113d4ecec33ae9471ec34313417e60
Martin Artishevsky <artishevsky.m@gmail.com>
2021-04-27 20:46:17 +0100

    Add models

---
+++ lib/models/extra.dart
@@ -0,0 +1,40 @@
+class Extra {
+  final int id;
+  final String imgUrl;
+  final String name;
+
+  Extra({this.id, this.imgUrl, this.name});
+}
+
+List<Extra> extras = [
+  Extra(
+    id: 1,
+    name: "Pepsi",
+    imgUrl: "assets/images/pepsi.png",
+  ),
+  Extra(
+    id: 2,
+    name: "Coke",
+    imgUrl: "assets/images/coke.png",
+  ),
+  Extra(
+    id: 3,
+    name: "Juice",
+    imgUrl: "assets/images/juice.png",
+  ),
+  Extra(
+    id: 4,
+    name: "Beef",
+    imgUrl: "assets/images/beef.png",
+  ),
+  Extra(
+    id: 5,
+    name: "Cookies",
+    imgUrl: "assets/images/cookies.png",
+  ),
+  Extra(
+    id: 6,
+    name: "Cheese",
+    imgUrl: "assets/images/cheese.png",
+  ),
+];
